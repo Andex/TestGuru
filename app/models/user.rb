3366c10d47bdf3eedb2262e5a509cd5e8b2a1e77
Andex <anyanov1998@gmail.com>
2020-12-19 00:04:03 +0300

    Add: инстанс-метод test_passed пользователя

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def test_passed(level)
+    Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
+        .where('test_passages.user_id = :id AND level = :level', id: id, level: level)
+  end
 end
