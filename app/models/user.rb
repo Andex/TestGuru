305647a26008eabf1f7d7a7fa4f9615ebcad20af
Andex <anyanov1998@gmail.com>
2020-12-19 13:25:21 +0300

    Fix: опечатка в названии таблицы test_passeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def test_passed(level)
-    Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
-        .where('test_passages.user_id = :id AND level = :level', id: id, level: level)
+    Test.joins('JOIN test_passeds ON tests.id = test_passeds.test_id')
+        .where('test_passeds.user_id = :id AND level = :level', id: id, level: level)
   end
 end
