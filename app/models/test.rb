cacf9600b7a6fbe6dd8b8d2f8e3e22625d52fb80
Andex <anyanov1998@gmail.com>
2020-12-20 09:54:05 +0300

    Revert "Add: таблица прохождений тестов пользователями и методов в test и user"
    This reverts commit 7ca3e8c83a9e08149261fb2027ef79d1ffc93041.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,2 @@
 class Test < ApplicationRecord
-  def self.test_names(category_title)
-    joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title = :category_title', category_title: category_title)
-      .order(title: :desc)
-      .pluck(:title)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,2 @@
 class User < ApplicationRecord
-  def test_passed(level)
-    Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
-        .where('test_passages.user_id = :id AND level = :level', id: id, level: level)
-  end
 end
