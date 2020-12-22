b5dfee229061d347959d976ed195b04604ab8f25
Andex <anyanov1998@gmail.com>
2020-12-22 19:03:43 +0300

    Add: ассоциации между моделями

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :test_passages
+  has_many :users, through: :test_passages
+
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+
   def self.test_names(category_title)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where('categories.title = :category_title', category_title: category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :test_passages
+  has_many :tests, through: :test_passages
+  has_many :created_test, class_name: 'Test'
+
   def test_passage(level)
     Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
         .where('test_passages.user_id = :id AND level = :level', id: id, level: level)
