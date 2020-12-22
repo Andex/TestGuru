67aab097c8a7095275737a37dc3b723e21b4441a
Andex <anyanov1998@gmail.com>
2020-12-22 18:46:24 +0300

    Add: внешний ключ user_id для tests (user, создавший тест)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_140022) do
+ActiveRecord::Schema.define(version: 2020_12_22_145459) do
 
   create_table 'answers', force: :cascade do |t|
     t.string 'body'
@@ -50,7 +50,9 @@ ActiveRecord::Schema.define(version: 2020_12_22_140022) do
     t.integer 'category_id'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'user_id'
     t.index ['category_id'], name: 'index_tests_on_category_id'
+    t.index ['user_id'], name: 'index_tests_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_12_22_140022) do
   add_foreign_key 'test_passages', 'tests'
   add_foreign_key 'test_passages', 'users'
   add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create!([{ name: 'Test_user', password: '12345' },
+                      { name: 'Test_user2', password: 'qwerty' }])
+
 categories = Category.create!([{ title: 'Спорт' },
                                { title: 'Музыка' },
                                { title: 'Математика' }])
 
-tests = Test.create!([{ title: 'О Формуле 1', level: 3, category_id: categories[0].id },
-                      { title: 'Авторы', level: 2, category_id: categories[1].id },
-                      { title: 'Посчитаем?', level: 0, category_id: categories[2].id }])
+tests = Test.create!([{ title: 'О Формуле 1', level: 3, category_id: categories[0].id, user_id: users[0] },
+                      { title: 'Авторы', level: 2, category_id: categories[1].id, user_id: users[0] },
+                      { title: 'Посчитаем?', level: 0, category_id: categories[2].id, user_id: users[0] }])
+
+tests_passages = TestPassage.create!([{ test_id: tests[0].id, user_id: users[0].id },
+                                      { test_id: tests[1].id, user_id: users[0].id },
+                                      { test_id: tests[1].id, user_id: users[1].id },
+                                      { test_id: tests[2].id, user_id: users[1].id }])
 
 questions = Question.create!([{ body: 'Назовите последнего чемпиона команды Ferrari', test_id: tests[0].id },
                               { body: 'Кто автор песни Bohemian Rhapsody?', test_id: tests[1].id },
@@ -31,11 +39,3 @@ answers = Answer.create!([{ body: 'Кими Райкконен', question_id: qu
                           { body: 'Формула 1', question_id: questions[0].id, correct: true },
                           { body: 'NASCAR', question_id: questions[0].id, correct: false },
                           { body: 'Формула 3', question_id: questions[0].id, correct: false }])
-
-users = User.create!([{ name: 'Test_user', password: '12345' },
-                      { name: 'Test_user2', password: 'qwerty' }])
-
-tests_passages = TestPassage.create!([{ test_id: tests[0].id, user_id: users[0].id },
-                                   { test_id: tests[1].id, user_id: users[0].id },
-                                   { test_id: tests[1].id, user_id: users[1].id },
-                                   { test_id: tests[2].id, user_id: users[1].id }])
