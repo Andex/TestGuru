8a0d6e500c04b3807810552bb5217409cb56a7d1
Andex <anyanov1998@gmail.com>
2020-12-22 19:20:05 +0300

    Refactor: методы с помощью ассоциаций

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,7 +7,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   def self.test_names(category_title)
-    joins('JOIN categories ON tests.category_id = categories.id')
+    joins(:category)
       .where('categories.title = :category_title', category_title: category_title)
       .order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :created_test, class_name: 'Test'
 
-  def test_passage(level)
-    Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
-        .where('test_passages.user_id = :id AND level = :level', id: id, level: level)
+  def test_passage_by_level(level)
+    tests.where(level: level)
   end
 end
