ce352f29bae79b9cf82c33d701bacf3295cfccfe
Andex <anyanov1998@gmail.com>
2020-12-23 23:36:24 +0300

    Fix: разрешение нулевого уровня, а не nil

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,7 +7,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   validates :title, presence: true, uniqueness: { scope: :level }
-  validates :level, numericality: { only_integer: true }, allow_nil: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :easy, -> { where(level: (0..1)) }
   scope :middle, -> { where(level: (2..4)) }
