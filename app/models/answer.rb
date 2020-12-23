f33cba8620f45c30502a3245c655449e3036653d
Andex <anyanov1998@gmail.com>
2020-12-23 23:35:01 +0300

    Fix: логичный перенос валидации из вопросов в ответы

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_max_answers
 
   scope :right_answers, -> { where(correct: true) }
+
+  private
+
+  def validate_max_answers
+    errors.add(:base) unless question.answers.ids.count <= 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,11 +4,4 @@ class Question < ApplicationRecord
   belongs_to :test
 
   validates :body, presence: true
-  validate :validate_max_questions
-
-  private
-
-  def validate_max_questions
-    errors.add(:answer) if answers.ids.count.positive? && answers.ids.count <= 4
-  end
 end
