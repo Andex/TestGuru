0e6841f0cb4c38b866829e2c0753182d2a01bcbc
Andex <anyanov1998@gmail.com>
2020-12-23 16:22:12 +0300

    Add: scope выбора тестов по уровню сложности

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,9 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+  scope :easy, -> { where(level: (0..1)) }
+  scope :middle, -> { where(level: (2..4)) }
+  scope :hard, -> { where(level: (5..Float::INFINITY)) }
   def self.test_names(category_title)
     joins(:category)
       .where('categories.title = :category_title', category_title: category_title)
