0070f90664dc3c18437184bc7f6904077b82e5ac
Andex <anyanov1998@gmail.com>
2020-12-24 13:42:16 +0300

    Add: уникальный составной индекс для теста title&level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_145459) do
+ActiveRecord::Schema.define(version: 2020_12_24_103750) do
 
   create_table 'answers', force: :cascade do |t|
     t.string 'body'
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_145459) do
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'user_id'
     t.index ['category_id'], name: 'index_tests_on_category_id'
+    t.index %w[title level], name: 'index_tests_on_title_and_level', unique: true
     t.index ['user_id'], name: 'index_tests_on_user_id'
   end
 
