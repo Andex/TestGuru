337c11dc40153671e603e55d70f7681a35d0dc8d
Andex <anyanov1998@gmail.com>
2020-12-26 20:26:00 +0300

    Add: ресурс questions, вложенный в ресурс tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
