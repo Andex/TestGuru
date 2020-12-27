46fdef18d4eaa585491d3cdc68a06931bd700bdf
Andex <anyanov1998@gmail.com>
2020-12-27 12:20:58 +0300

    Add: show, create и приватных поиска вопроса и теста

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,39 @@
 class QuestionsController < ApplicationController
 
+  before_action :find_test, only: ['create']
+  before_action :find_question, only: ['show']
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  # def show
-  #   render json:
-  # end
-
-  def new
-  end
 
   def index
     render plain: params.inspect
   end
 
+  def show
+    render plain: @question.body
+  end
+
+  def new; end
+
+  def create
+    @question = @test.questions.new(question_params)
+    @question.save
+  end
+
   private
 
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
   def rescue_with_question_not_found
-    render plain: "Вопрос не найден"
+    render plain: 'Вопрос не найден'
   end
 end
