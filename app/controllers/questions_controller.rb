a7a7d3f1e17a4d2020a5aff39415ad978dd235e4
Andex <anyanov1998@gmail.com>
2020-12-27 11:53:13 +0300

    Add: обработка исключения - вопрос не найден

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,20 @@
 class QuestionsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
   # def show
   #   render json:
   # end
 
   def new
+  end
+
+  def index
+    render plain: params.inspect
+  end
+
+  private
+
+  def rescue_with_question_not_found
+    render plain: "Вопрос не найден"
   end
 end
