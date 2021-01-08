7c729dd1f8cf6e8f09066e353c718ee0cf2e32bf
Andex <anyanov1998@gmail.com>
2021-01-08 18:12:53 +0300

    Fix: вывод тестов

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,16 +1,18 @@
 class TestsController < ApplicationController
 
+  before_action :find_test, only: %i[show]
+
   def index
-    @tests = Test.all
+    render json: { tests: Test.all }
   end
 
   def show
     render inline: '<%= @test.title %>'
   end
-  #
-  # private
-  #
-  # def find_test
-  #   @test = Test.find(params[:id])
-  # end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
 end
