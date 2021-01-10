b7012a24b41bf87f66f79cef1992e1dfb81e0995
Andex <anyanov1998@gmail.com>
2021-01-11 00:45:15 +0300

    Add: футер с инф-цией о проекте, авторе и ссылками на них

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    link_to link_to 'Проект TestGuru', "http://github.com/" + author + "/" + repo
+  end
 end
