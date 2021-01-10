516c58cc21d7858323015e4daf3abe4987f5e8e3
Andex <anyanov1998@gmail.com>
2021-01-10 19:56:20 +0300

    Add: все стандартные действия с представлениями

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,18 +1,50 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: %i[show]
+  before_action :find_test, only: %i[show edit update]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    render json: { tests: Test.all }
+    @tests = Test.all
   end
 
-  def show
-    render inline: '<%= @test.title %>'
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
   end
 
   private
 
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
+  end
+
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Тест не найден'
   end
 end
