ede033dec3a2cb851089722f99e64061ed9daa60
Andex <anyanov1998@gmail.com>
2021-01-10 23:28:01 +0300

    Add: хелпер заголовка формы создания и редактирования вопроса - question_header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def question_header(question)
+    if question.new_record?
+      "Создание вопроса для теста #{question.test.title}"
+    else
+      "Редактирование вопроса теста #{question.test.title}"
+    end
+  end
 end
