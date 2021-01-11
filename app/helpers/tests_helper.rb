667cf262f01aa6a5856daf8ccc78134d62860d79
Andex <anyanov1998@gmail.com>
2021-01-11 13:04:51 +0300

    Fix: лишний метод замен на доступ через ассоциацию

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -3,11 +3,7 @@ module TestsHelper
   TEST_LEVELS = { 0 => :easy, 1 => :easy, 2 => :middle, 3 => :middle, 4 => :middle, 5 => :hard }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || :hero
-  end
-
-  def test_category(test)
-    Category.all.find(test.category_id.to_s).title
+    TEST_LEVELS[test.level] || :hard
   end
 
   def number_of_questions(test)
