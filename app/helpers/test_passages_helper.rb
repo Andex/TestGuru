61ffc94b5f71a65b76093e1cc6ed754fbefc8fda
Andex <anyanov1998@gmail.com>
2021-01-14 00:04:22 +0300

    Add: вывод сообщения об успешности тестирования в процентах

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -3,4 +3,8 @@ module TestPassagesHelper
   def question_number(question)
     question.test.questions.order(:id).where('id <= ?', question.id).count
   end
+
+  def result(test_passage)
+    100 / test_passage.test.questions.count * test_passage.correct_questions
+  end
 end
