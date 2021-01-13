a127d69c3b8978d24d38c66cef753ebb2b712630
Andex <anyanov1998@gmail.com>
2021-01-13 22:30:52 +0300

    Add: методы для прохождения вопроса в тесте

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,2 +1,23 @@
 class TestPassagesController < ApplicationController
+
+  before_action :set_test_passage, only: %i[show update]
+
+  def show
+
+  end
+
+  def result
+
+  end
+
+  def update
+    @test_passage.accept!(params[:answers_ids])
+    render :show
+  end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,11 +3,30 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', foreign_key: :question_id, optional: true
 
-  before_validation :before_validation_set_first_question
+  before_validation :before_validation_set_first_question, on: :create
+
+  def accept!(answers_ids)
+    self.correct_questions += 1 if correct_answer?(answers_ids)
+
+    self.current_question = next_question
+    save!
+  end
 
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answers_ids)
+    correct_answers.ids.sort == answers_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.right_answers
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
