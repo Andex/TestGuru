83da22a3578152f1b3c24ca3ab27e708bab12d81
Andex <anyanov1998@gmail.com>
2021-01-15 00:03:12 +0300

    Refactor: вынос аутентификации в модуль

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,11 @@
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_test, class_name: 'Test'
-
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, if: Proc.new{ |u| u.password_digest.blank? }
-  validates :password, confirmation: true
 
   def test_passage_by_level(level)
     tests.where(level: level)
@@ -20,24 +14,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
