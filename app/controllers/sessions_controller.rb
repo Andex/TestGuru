326dbad01b276ebff0dfa4edcea16fb8e040b039
Andex <anyanov1998@gmail.com>
2021-01-15 21:37:38 +0300

    Add: flash-сообщение просьбы подтвердить учетную запись

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Подтвердите свою учетную запись, пожалуйста'
       render :new
     end
   end
