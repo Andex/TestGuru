0ccd2772db0e2d90a348eb201c1c4ed21ca636f2
Andex <anyanov1998@gmail.com>
2021-01-16 15:06:53 +0300

    Add: наличие, формат name и email + уникальность

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,20 @@
 
 class User < ApplicationRecord
 
+  NAME_VALID = /\A[a-zA-z]+\w*/.freeze
+  EMAIL_VALID = /(\w|-)+@\w+\.\w+/.freeze
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_test, class_name: 'Test'
 
   has_secure_password
+
+  validates :name, presence: true,
+                   format: { with: NAME_VALID, message: 'не должно начинаться с цифры или содержать только цифры' }
+  validates :email, presence: true,
+                    format: { with: EMAIL_VALID, message: 'некорректен' },
+                    uniqueness: true
 
   def test_passage_by_level(level)
     tests.where(level: level)
