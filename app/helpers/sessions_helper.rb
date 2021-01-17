4decf86fc4b18306e202cad2184bb1d0cb55bdac
Andex <anyanov1998@gmail.com>
2021-01-17 14:52:47 +0300

    Fix: flash-сообщения не отражались из-за опечатки

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
   def flash_message(type)
-    content_tag :p, flash[type], class: "flash #{type}" if flash[:type]
+    content_tag :p, flash[type], class: "flash #{type}" if flash[type]
   end
 end
