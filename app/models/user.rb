5dfbed11d31b59a13c0bdeaa58c3e54ff8e50291
Andex <anyanov1998@gmail.com>
2021-01-19 16:27:19 +0300

    Add: при регистрации полей first и last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,17 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
-  # NAME_VALID = /\A[a-zA-z]+\w*/.freeze
+  NAME_VALID = /\A[a-zA-Zа-яА-Я]+\w*/.freeze
   EMAIL_VALID = /(\w|-)+@\w+\.\w+/.freeze
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test'
 
-  # validates :name, presence: true,
-  #                  format: { with: NAME_VALID, message: 'не должно начинаться с цифры или содержать только цифры' }
+  validates :first_name, presence: true,
+                         format: { with: NAME_VALID, message: 'не должно начинаться с цифры или содержать только цифры' }
+  validates :last_name, presence: true,
+                        format: { with: NAME_VALID, message: 'не должно начинаться с цифры или содержать только цифры' }
   validates :email, presence: true,
                     format: { with: EMAIL_VALID, message: 'некорректен' },
                     uniqueness: true
