2c0a69ed76fa3b3c38653c4a6a4344fada650a07
Andex <anyanov1998@gmail.com>
2021-01-20 00:11:30 +0300

    Add: определение локали по входящему запросу

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
 
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Здравствуйте, #{current_user.first_name} #{current_user.last_name}!"
