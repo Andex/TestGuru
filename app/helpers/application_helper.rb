cd604518e38fe19c083e03dd1ca950ac35277203
Andex <anyanov1998@gmail.com>
2021-01-21 18:35:05 +0300

    Add: локализация подвала сайта

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to 'Проект TestGuru', "http://github.com/#{author}/#{repo}"
+    link_to t('helpers.githab_url.name_project'), "http://github.com/#{author}/#{repo}"
   end
 end
