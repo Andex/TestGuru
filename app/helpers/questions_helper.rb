1a290e9c69cc9f0211816984869615bd200749a7
Andex <anyanov1998@gmail.com>
2021-01-23 22:24:21 +0300

    Add: локализация страницы создания и редактирования вопроса теста

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,2 @@
 module QuestionsHelper
-
-  def question_header(question)
-    if question.new_record?
-      "Создание вопроса для теста #{question.test.title}"
-    else
-      "Редактирование вопроса теста #{question.test.title}"
-    end
-  end
 end
