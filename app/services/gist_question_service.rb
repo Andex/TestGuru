3e074ffec7ff125e43cc87886950d2c608dfa0a0
Andex <anyanov1998@gmail.com>
2021-01-26 18:07:51 +0300

    Fix: локализация параметров gist GistQuestionService

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -14,7 +14,7 @@ class GistQuestionService
 
   def gist_params
     {
-      description: t('services.description', title: @test.title),
+      description: I18n.t('services.description', title: @test.title),
       files: {
         'test-guru-question.txt' => {
           content: gist_content
