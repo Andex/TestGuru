5ea146f6aa6896c6d812973830f809ddb6d24d0a
Andex <anyanov1998@gmail.com>
2021-01-26 23:44:07 +0300

    Add: модель Gist

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test'
+  has_many :gists
 
   validates :first_name, presence: true,
                          format: { with: NAME_VALID, message: 'не должно начинаться с цифры или содержать только цифры' }
