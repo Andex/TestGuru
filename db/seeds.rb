ea67ccd3319c35e2df3e4e2135367360f7fbfbb4
Andex <anyanov1998@gmail.com>
2021-02-02 15:27:54 +0300

    Refactor: переход с sqlite на postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-# users = User.create!([{ first_name: 'Иван', last_name: 'Тест', email: 'test_user@mail.ru', password: '121212', confirmed_at: Date.today },
-#                       { first_name: 'Алиса', last_name: 'Тест', email: 'test_user2@mail.ru', password: 'qwerty', confirmed_at: Date.today }])
-users = User.all
+users = User.create!([{ first_name: 'Иван', last_name: 'Тест', email: 'test_user@mail.ru', password: '121212', type: 'Admin'},
+                      { first_name: 'Алиса', last_name: 'Тест', email: 'test_user2@mail.ru', password: 'qwerty'}])
 
 categories = Category.create!([{ title: 'Спорт' },
                                { title: 'Музыка' },
