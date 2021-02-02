493f63de3be23cd38fd584c396c995aba0c185be
Andex <anyanov1998@gmail.com>
2021-02-02 22:23:23 +0300

    Add: локализация уровня тестов

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,9 +1,14 @@
 module TestsHelper
 
-  TEST_LEVELS = { 0 => :easy, 1 => :easy, 2 => :middle, 3 => :middle, 4 => :middle, 5 => :hard }.freeze
+  TEST_LEVELS = { 0 => I18n.t('.helpers.test_levels.easy'),
+                  1 => I18n.t('.helpers.test_levels.easy'),
+                  2 => I18n.t('.helpers.test_levels.middle'),
+                  3 => I18n.t('.helpers.test_levels.middle'),
+                  4 => I18n.t('.helpers.test_levels.middle'),
+                  5 => I18n.t('.helpers.test_levels.hard') }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || :hard
+    TEST_LEVELS[test.level] || t('.helpers.test_levels.hard')
   end
 
   def number_of_questions(test)
