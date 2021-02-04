26e4edd788ad975e4449dc488043720b998ca3f3
Andex <anyanov1998@gmail.com>
2021-02-04 18:05:02 +0300

    Add: тема письма обратной связи админу

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -3,6 +3,6 @@ class FeedbacksMailer < ApplicationMailer
   def send_feedback(feedback)
     @feedback = feedback
 
-    mail to: Admin.first.email, feedback: @feedback
+    mail to: Admin.first.email, feedback: @feedback, subject: (@feedback[:topic]).to_s
   end
 end
