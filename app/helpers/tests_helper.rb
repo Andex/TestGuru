53f7bb715ad1c1ede323cc945bbd647918d9f675
Andex <anyanov1998@gmail.com>
2021-02-04 18:29:16 +0300

    Fix: локализация уровней тестов

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,14 +1,14 @@
 module TestsHelper
 
-  TEST_LEVELS = { 0 => I18n.t('.helpers.test_levels.easy'),
-                  1 => I18n.t('.helpers.test_levels.easy'),
-                  2 => I18n.t('.helpers.test_levels.middle'),
-                  3 => I18n.t('.helpers.test_levels.middle'),
-                  4 => I18n.t('.helpers.test_levels.middle'),
-                  5 => I18n.t('.helpers.test_levels.hard') }.freeze
+  TEST_LEVELS = { 0 => I18n.t('helpers.test_levels.easy'),
+                  1 => I18n.t('helpers.test_levels.easy'),
+                  2 => I18n.t('helpers.test_levels.middle'),
+                  3 => I18n.t('helpers.test_levels.middle'),
+                  4 => I18n.t('helpers.test_levels.middle'),
+                  5 => I18n.t('helpers.test_levels.hard') }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || t('.helpers.test_levels.hard')
+    TEST_LEVELS[test.level] || I18n.t('helpers.test_levels.hard')
   end
 
   def number_of_questions(test)
