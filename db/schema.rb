10de98ba45e01aa14bcca234024737d74ea688cb
Andex <anyanov1998@gmail.com>
2021-02-08 23:39:53 +0300

    Fix: названия таблиц отношений Badge с User и TestPassage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_155707) do
+ActiveRecord::Schema.define(version: 2021_02_08_191702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -24,22 +24,13 @@ ActiveRecord::Schema.define(version: 2021_02_07_155707) do
     t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table 'badge_test_passages_tables', force: :cascade do |t|
+  create_table 'badge_test_passages', force: :cascade do |t|
     t.bigint 'test_passages_id', null: false
     t.bigint 'badge_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
-    t.index ['badge_id'], name: 'index_badge_test_passages_tables_on_badge_id'
-    t.index ['test_passages_id'], name: 'index_badge_test_passages_tables_on_test_passages_id'
-  end
-
-  create_table 'badge_users_tables', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'badge_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['badge_id'], name: 'index_badge_users_tables_on_badge_id'
-    t.index ['user_id'], name: 'index_badge_users_tables_on_user_id'
+    t.index ['badge_id'], name: 'index_badge_test_passages_on_badge_id'
+    t.index ['test_passages_id'], name: 'index_badge_test_passages_on_test_passages_id'
   end
 
   create_table 'badges', force: :cascade do |t|
@@ -49,6 +40,15 @@ ActiveRecord::Schema.define(version: 2021_02_07_155707) do
     t.string 'parameter', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'badges_users', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'badge_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['badge_id'], name: 'index_badges_users_on_badge_id'
+    t.index ['user_id'], name: 'index_badges_users_on_user_id'
   end
 
   create_table 'categories', force: :cascade do |t|
@@ -126,10 +126,10 @@ ActiveRecord::Schema.define(version: 2021_02_07_155707) do
   end
 
   add_foreign_key 'answers', 'questions'
-  add_foreign_key 'badge_test_passages_tables', 'badges'
-  add_foreign_key 'badge_test_passages_tables', 'test_passages', column: 'test_passages_id'
-  add_foreign_key 'badge_users_tables', 'badges'
-  add_foreign_key 'badge_users_tables', 'users'
+  add_foreign_key 'badge_test_passages', 'badges'
+  add_foreign_key 'badge_test_passages', 'test_passages', column: 'test_passages_id'
+  add_foreign_key 'badges_users', 'badges'
+  add_foreign_key 'badges_users', 'users'
   add_foreign_key 'gists', 'questions'
   add_foreign_key 'gists', 'users'
   add_foreign_key 'questions', 'tests'
