2bef79526f867f50a5721d2232c5728018217489
Andex <anyanov1998@gmail.com>
2021-02-08 21:32:36 +0300

    Add: вывод всех бейджей на панель управления бейджами для админа

---
+++ app/controllers/admin/badges_controller.rb
@@ -0,0 +1,26 @@
+class Admin::BadgesController < Admin::BaseController
+
+  def index
+    @badges = Badge.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
