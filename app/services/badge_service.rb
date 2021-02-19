54189520181c8e2595ae02814521af1317d1da22
Andex <anyanov1998@gmail.com>
2021-02-20 00:02:40 +0300

    Add: выдача бейджа за прохождение всех тестов заданного уровня

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -1,5 +1,12 @@
 class BadgeService
 
+  TEST_LEVELS = { 0 => 'easy',
+                  1 => 'easy',
+                  2 => 'middle',
+                  3 => 'middle',
+                  4 => 'middle',
+                  5 => 'hard' }.freeze
+
   def initialize(test_passage)
     @user = test_passage.user
     @test_passage = test_passage
@@ -11,22 +18,21 @@ class BadgeService
     end
   end
 
+  private
+
   def give_badge(badge)
     @user.badges.push(badge)
     @test_passage.badges.push(badge)
   end
 
-  private
-
   def all_tests_in_category(badge, category)
-    return if category != @test_passage.test.category.title || @user.badges.find(badge.id)
-
-    id_tests_in_category = Test.category_tests(@test_passage.test.category).pluck(:id)
-    id_tests_user_in_category = []
-    id_tests_in_category.each do |test_id|
-      id_tests_user_in_category << test_id if @user.test_passages.where('test_id = ? AND passed = ?', test_id, true)
+    begin
+      return if category != @test_passage.test.category.title || @user.badges.find(badge.id)
+    rescue ActiveRecord::RecordNotFound
+      nil
     end
-    id_tests_user_in_category == id_tests_in_category
+    id_tests_in_category = Test.category_tests(@test_passage.test.category).pluck(:id)
+    check_for_matches(id_tests_in_category)
   end
 
   def pass_on_first_try(badge, attempt)
@@ -34,8 +40,24 @@ class BadgeService
   end
 
   def all_tests_at_level(badge, level)
-    # level = badge.parameter
-
+    begin
+      return if level != test_level(@test_passage.test) || @user.badges.find(badge.id)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+    tests_at_level = Test.send(level.to_s).pluck(:id)
+    check_for_matches(tests_at_level)
   end
 
+  def check_for_matches(tests_id)
+    user_tests = []
+    tests_id.each do |test_id|
+      user_tests << test_id if @user.test_passages.where('test_id = ? AND passed = ?', test_id, true)
+    end
+    user_tests == tests_id
+  end
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || 'hard'
+  end
 end
